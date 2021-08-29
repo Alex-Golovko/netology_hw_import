a4bcc690b454dd65d47bfcf9e13703712ddb2db6
ya.golovko-alex2017@yandex.ru <Olga121082>
2021-08-29 17:52:38 +0300

    1

--- packages/main.py
+++ packages/main.py
@@ -2,7 +2,9 @@ from application.salary import calculate_salary
 from application.db.people import get_employees
 import datetime
 
+
 if __name__ == '__main__':
+ 
     print(datetime.date.today())
     calculate_salary()
     get_employees()
